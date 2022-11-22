f1a9689c2af4b35808d379bbb99953498c0179da
Juliano Mohr <jbm@redhat.com>
2022-11-22 10:55:59 +0000

    Add health live and ready checks

--- coolstore-legacy/src/main/java/com/redhat/coolstore/ExceptionHandlingController.java
+++ coolstore-legacy/src/main/java/com/redhat/coolstore/ExceptionHandlingController.java
@@ -1,5 +1,6 @@
 package com.redhat.coolstore;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import java.util.logging.Logger;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,5 +17,13 @@ public class ExceptionHandlingController {
     public void resourceNotFound(final ResourceNotFoundException e) {
 
         log.warning("Resource not found");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = INTERNAL_SERVER_ERROR, reason = "Internal server error")
+    public void resourceNotFound(final Exception e) {
+
+        log.severe("Internal server error: " + e.getMessage());
+        e.printStackTrace();
     }
 }
--- coolstore-legacy/src/main/java/com/redhat/coolstore/orders/OrderRepository.java
+++ coolstore-legacy/src/main/java/com/redhat/coolstore/orders/OrderRepository.java
@@ -3,5 +3,5 @@ package com.redhat.coolstore.orders;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import com.redhat.coolstore.entity.Order;
 
-public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
+interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
 }
